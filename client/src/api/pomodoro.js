d41a9496f00ad31524cbbca6720c56f9e7c4a443
MohamedBoghdaddy <mohamedhammad3.142@gmail.com>
2026-04-12 12:48:56 +0200

    feat(api): add adaptive pomodoro helpers

--- client/src/api/pomodoro.js
+++ client/src/api/pomodoro.js
@@ -1,5 +1,7 @@
 import client from './client';
-export const getActive = () => client.get('/pomodoro/active').then(r => r.data);
-export const getHistory = p => client.get('/pomodoro/history', { params: p }).then(r => r.data);
-export const startSession = d => client.post('/pomodoro/start', d).then(r => r.data);
-export const stopSession = (id, d) => client.post(`/pomodoro/${id}/stop`, d).then(r => r.data);
+export const getActive              = ()  => client.get('/pomodoro/active').then(r => r.data);
+export const getHistory             = p   => client.get('/pomodoro/history', { params: p }).then(r => r.data);
+export const startSession           = d   => client.post('/pomodoro/start', d).then(r => r.data);
+export const stopSession            = (id, d) => client.post(`/pomodoro/${id}/stop`, d).then(r => r.data);
+export const getAdaptiveRecommendations = () => client.get('/pomodoro/adaptive').then(r => r.data);
+export const getBestTimes           = ()  => client.get('/pomodoro/best-times').then(r => r.data);
