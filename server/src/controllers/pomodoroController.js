7dde147e11a0d45bb2750bf8228e6f8745a1ddbe
MohamedBoghdaddy <mohamedhammad3.142@gmail.com>
2026-04-12 12:48:53 +0200

    feat(pomodoro): record habits after completed sessions

--- server/src/controllers/pomodoroController.js
+++ server/src/controllers/pomodoroController.js
@@ -1,4 +1,5 @@
 const pomodoroService = require('../services/pomodoro/pomodoroService');
+const habitService    = require('../services/habits/habitService');
 const { asyncHandler } = require('../middleware/errorHandler');
 
 const getWorkspaceId = (req) => req.user.defaultWorkspaceId?.toString() || req.query.workspaceId;
@@ -11,6 +12,11 @@ const startSession = asyncHandler(async (req, res) => {
 const stopSession = asyncHandler(async (req, res) => {
   const session = await pomodoroService.stopSession(getWorkspaceId(req), req.user._id, req.params.id, req.body);
   res.json(session);
+
+  // After completing a focus session, update habit data (fire and forget)
+  if (req.body.status === 'completed') {
+    habitService.recordDailyHabit(req.user._id, getWorkspaceId(req)).catch(() => {});
+  }
 });
 
 const getHistory = asyncHandler(async (req, res) => {
